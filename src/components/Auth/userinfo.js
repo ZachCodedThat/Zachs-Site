47e551dd5052960ae4363a1108dce66ab213d78b
ZachCodedThat <Zachemailsback@gmail.com>
2021-06-22 21:03:34 -0400

    Login button hides when logged in

--- src/components/Auth/userinfo.js
+++ src/components/Auth/userinfo.js
@@ -1,26 +1,22 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-
-function Userinfo()  {
+function Userinfo() {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
     return <div>Loading ...</div>;
   }
 
-  return (
-    isAuthenticated ?
-      <div className="userData">
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
-      :<div>Not Logged in</div>
-    
-    
-  
+  return isAuthenticated ? (
+    <div className="userData">
+      <img src={user.picture} alt={user.name} />
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+    </div>
+  ) : (
+    <div>Not Logged in</div>
   );
-};
+}
 
 export default Userinfo;
