a80f938b5c7103bf44cb565ec192fbd233159547
ZachCodedThat <Zachemailsback@gmail.com>
2021-06-22 21:05:57 -0400

    log in button hides when logged in

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,29 @@ import React from "react";
 import Login from "../components/Auth/login";
 import Logout from "../components/Auth/logout";
 import Userinfo from "../components/Auth/userinfo";
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Home() {
-  return (
+  const { isAuthenticated } = useAuth0();
+
+  return !isAuthenticated ? (
     <>
-    <div className="login">
-      <Login />
-      <Logout />
-    </div>
-    <div className="profile"><Userinfo />
-    </div>
+      <div className="login">
+        <Login />
+        <Logout />
+      </div>
+      <div className="profile">
+        <Userinfo />
+      </div>
+    </>
+  ) : (
+    <>
+      <div className="login">
+        <Logout />
+      </div>
+      <div className="profile">
+        <Userinfo />
+      </div>
     </>
   );
 }
