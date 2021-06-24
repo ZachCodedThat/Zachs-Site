affd8de9cb8b708091d8b71c140ffbd4441aa340
ZachCodedThat <Zachemailsback@gmail.com>
2021-06-24 00:45:54 -0400

    added protected route

--- src/components/Auth/userinfo.js
+++ src/components/Auth/userinfo.js
@@ -9,7 +9,7 @@ function Userinfo() {
   }
 
   return isAuthenticated ? (
-    <div className="userData">
+    <div>
       <img src={user.picture} alt={user.name} />
       <h2>{user.name}</h2>
       <p>{user.email}</p>
--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,7 @@ import Home from "../pages/Home";
 import Profile from "../pages/Profile";
 import Projects from "../pages/Projects";
 import { Auth0Provider } from "@auth0/auth0-react";
+import ProtectedRoute from "./Auth/protected-route";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
@@ -13,25 +14,24 @@ const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const App = () => {
   return (
     <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    redirectUri={window.location.origin}
-  >
-    <div>
-      <>
-        <BrowserRouter>
-          <Nav />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/Profile" exact component={Profile} />
-            <Route path="/Projects" exact component={Projects} />
-          </Switch>
-        </BrowserRouter>
-      </>
-    </div>
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+      <div>
+        <>
+          <BrowserRouter>
+            <Nav />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <ProtectedRoute path="/Profile" exact component={Profile} />
+              <Route path="/Projects" exact component={Projects} />
+            </Switch>
+          </BrowserRouter>
+        </>
+      </div>
     </Auth0Provider>
   );
 };
 
 render(<App />, document.getElementById("root"));
-
